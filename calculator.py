45e9b34a5b0a8a55c87d1662fb563e1dc6823ae6
fatredbeaver <tedmundho@gmail.com>
2019-11-14 16:50:45 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -7,7 +7,10 @@ def subtract(x, y):
 
 
 def divide(x, y):
-    return x / y
+    if y == 0:
+        return 'Invalid value for denominator'
+    else:
+        return x / y
 
 
 def multiply(x, y):
